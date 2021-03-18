1b78ea45797e4102b84a442297857c2dd9281657
bianca.severo <bianca.severo@softplan.com.br>
2021-03-18 20:38:52 -0300

    Aula 32 - Card List Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.components';
+
 import './App.css'
 
 class App extends Component {
@@ -10,19 +13,20 @@ class App extends Component {
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users}));
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name} </h1>
+        <CardList name='Hehe'>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
           ))}
+        </CardList>
       </div>
     )
   }
---
+++ src/components/card-list/card-list.components.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './card-list.styles.css'
+
+//rfce
+
+export const CardList = props => {
+    return <div className="card-list">{props.children}</div>
+}
