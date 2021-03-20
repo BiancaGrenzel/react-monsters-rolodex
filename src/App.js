fc1d24495ac27ff650e3ff6c58e779772f5c783e
bianca.severo <bianca.severo@softplan.com.br>
2021-03-20 20:37:17 -0300

    Aula 41 - Sarch Box Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.components';
 
+import { SearchBox } from './components/search-box/search-box.component'
+
 import './App.css'
 
 class App extends Component {
@@ -22,24 +24,19 @@ class App extends Component {
 
   render() {
 
-    
     // Same energy
     // const monsters = this.state.monsters;
     // const searchField = this.state.searchField;
 
     const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => 
+    const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-      );
+    );
 
     return (
       <div className="App">
-        <input
-          type='search'
-          placeholder='search monsters'
-          onChange={e =>
-            this.setState({ searchField: e.target.value })}>
-        </input>
+        <SearchBox placeholder='search monsters'
+          handleChange={e => this.setState({ searchField: e.target.value })} />
         <CardList monsters={filteredMonsters}>
         </CardList>
       </div>
