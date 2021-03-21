0ef622f073e497a59f804245b8b824d1360c3afb
bianca.severo <bianca.severo@softplan.com.br>
2021-03-20 21:14:39 -0300

    Aula 48 - Deploying Our App

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="title"> Monsters Rolodex </h1>
         <SearchBox placeholder='search monsters'
           handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters}>
